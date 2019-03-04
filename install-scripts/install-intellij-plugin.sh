7cdfed87b0a0f9a06dec2002794a06b9d1194347
Emeric MARTINEAU <11473190+bubulemaster@users.noreply.github.com>
2019-03-04 19:02:00 +0100

    Get URI file of plugin for IntelliJ

--- install-scripts/install-intellij-plugin.sh
+++ install-scripts/install-intellij-plugin.sh
@@ -6,7 +6,7 @@
 #
 # print url
 get_plugin_url() {
-  local plugin_marketplace_url="https://plugins.jetbrains.com/search/suggest?product=idea_ce&term="
+  local plugin_marketplace_url="${JET_BRAIN_PLUGINS_URL}/search/suggest?product=idea_ce&term="
   local filename=/tmp/plugin_marketplace.json
 
   curl "${plugin_marketplace_url}$1" --output ${filename} 2>/dev/null
@@ -44,57 +44,104 @@ get_number() {
   echo ${v:-0}
 }
 
+# Check if current version of plugin compatible with IntelliJ version.
+#
+# $1 Current IntelliJ version
+# $2 Version of IntelliJ supported by plugin
+#
+# Set environment variable IS_COMPATIBLE
 check_compatible_version() {
-  local mini_version=$(echo "$2" | cut -d ' ' -f 1)
-  local mini_major_version=$(get_number ${mini_version} 1)
-  local mini_minor_version=$(get_number ${mini_version} 2)
-  local mini_fix_version=$(get_number ${mini_version} 3)
+  local mini_version="$(echo $2 | cut -d '-' -f 1 | xargs)"
+  local mini_major_version=$(get_number "${mini_version}" 1)
+  local mini_minor_version=$(get_number "${mini_version}" 2)
+  local mini_fix_version=$(get_number "${mini_version}" 3)
 
-  local max_version=$(echo "$2" | cut -d ' ' -f 3)
-  local max_major_version=$(get_number ${max_version} 1)
-  local max_minor_version=$(get_number ${max_version} 2)
-  local max_fix_version=$(get_number ${max_version} 3)
+  # In JSON, version are separated by UTF8 character
+  local max_version="$(echo $2 | cut -d '-' -f 2 | xargs)"
+  local max_major_version=$(get_number "${max_version}" 1)
+  local max_minor_version=$(get_number "${max_version}" 2)
+  local max_fix_version=$(get_number "${max_version}" 3)
 
-  echo $mini_major_version $mini_minor_version $mini_fix_version
-  echo $max_major_version $max_minor_version $max_fix_version
+  local current_major_version=$(get_number ${1} 1)
+  local current_minor_version=$(get_number ${1} 2)
+  local current_fix_version=$(get_number ${1} 3)
+
+  # Our IntelliJ version is upper or equal of minimum
+  if
+    # Our IntelliJ major version is upper or equal of minimum
+    ([ ${mini_major_version} -lt ${current_major_version} ] ||
+    [ ${mini_major_version} -eq ${current_major_version} ]) &&
+    # Our IntelliJ major version is less or equal of maximum
+    ([ ${max_major_version} -gt ${current_major_version} ] ||
+    [ ${max_major_version} -eq ${current_major_version} ]) &&
+    # Our IntelliJ minor version is upper or equal of minimum
+    ([ ${mini_minor_version} -lt ${current_minor_version} ] ||
+     [ ${mini_minor_version} -eq ${current_minor_version} ]) &&
+    # Our IntelliJ minor version is less or equal of maximum
+    ([ ${max_minor_version} -gt ${current_minor_version} ] ||
+     [ ${max_minor_version} -eq ${current_minor_version} ]) &&
+     # Our IntelliJ fix version is upper or equal of minimum
+     ([ ${mini_fix_version} -lt ${current_fix_version} ] ||
+      [ ${mini_fix_version} -eq ${current_fix_version} ]) &&
+     # Our IntelliJ fix version is less or equal of maximum
+     ([ ${max_fix_version} -gt ${current_fix_version} ] ||
+      [ ${max_fix_version} -eq ${current_fix_version} ])
+    then
+    IS_COMPATIBLE="true"
+  else
+    IS_COMPATIBLE="false"
+  fi
 }
 
 # Get last version of plugin for IntelliJ version.
 #
 # $1 IntelliJ version
 # $2 Id of plugin
+#
+# Set environment PLUGIN_DOWNLOAD_URL
 get_last_plugin_version() {
-  local plugin_marketplace_url="https://plugins.jetbrains.com/api/plugins/$2/updates?channel="
+  local plugin_marketplace_url="${JET_BRAIN_PLUGINS_URL}/api/plugins/$2/updates?channel="
   local filename=/tmp/plugin.json
 
   curl "${plugin_marketplace_url}" --output ${filename} 2>/dev/null
 
   local index=0
-  local plugin_version="$(cat ${filename} | jq '.['${index}'].compatibleVersions.IDEA_COMMUNITY' | xargs)"
+  local plugin_version="$(cat ${filename} | jq '.['${index}'].version' | xargs)"
+  local plugin_compat_version="$(cat ${filename} | jq '.['${index}'].compatibleVersions.IDEA_COMMUNITY' | xargs)"
   local plugin_url='null'
 
   # If 'null', index doesn't exists
-  while [ "null" != "${plugin_version}" ]; do
-    check_compatible_version "$1" "${plugin_version}"
+  while [ "null" != "${plugin_compat_version}" ]; do
+    echo -n "  - Check if version '${plugin_version}' is compatible"
 
-    if [ "$1" = "???" ]; then
-      plugin_url="???"
+    # Clean utf8 code and 'build ' prefix
+    plugin_compat_version="$(echo ${plugin_compat_version} | sed 's/—/-/' | sed 's/build //')"
+    check_compatible_version "$1" "${plugin_compat_version}"
+
+    if [ "${IS_COMPATIBLE}" = "true" ]; then
+      plugin_url="$(cat ${filename} | jq '.['${index}'].file' | xargs)"
+
+      print_ok
+
       # To break while
-      plugin_version='null'
+      plugin_compat_version='null'
     else
+      print_ko
+
       index=$(expr ${index} + 1)
-      plugin_version="$(cat ${filename} | jq '.['${index}'].compatibleVersions.IDEA_COMMUNITY' | xargs)"
+      local plugin_version="$(cat ${filename} | jq '.['${index}'].version' | xargs)"
+      local plugin_compat_version="$(cat ${filename} | jq '.['${index}'].compatibleVersions.IDEA_COMMUNITY' | xargs)"
     fi
   done
 
   rm -f "${filename}"
 
-  echo ${plugin_url}
+  PLUGIN_DOWNLOAD_URL="${plugin_url}"
 }
 
 install_plugin() {
   for plugin in ${1}; do
-    echo -n "Download IntelliJ plugin '${plugin}' "
+    echo "Download IntelliJ plugin '${plugin}' "
 
     local plugin_url="$(get_plugin_url ${plugin})"
 
@@ -104,7 +151,8 @@ install_plugin() {
     else
       local plugin_id="$(echo ${plugin_url} | cut -d '-' -f 1 | cut -d '/' -f 3)"
       get_last_plugin_version "${INTELLIJ_VERSION}" "${plugin_id}"
-      print_ok
+
+      # Download plugin https://plugins.jetbrains.com/files/"${PLUGIN_DOWNLOAD_URL}"
     fi
   done
 }
@@ -114,6 +162,17 @@ BASEDIR="$(dirname ${REALPATH})"
 
 . "${BASEDIR}/../config.cfg"
 . "${BASEDIR}/common.sh"
+
+# Check if apm is installed
+if [ -z "$(command -v jq)" ]; then
+  . "${BASEDIR}/common.sh"
+
+  echo -n "Jq not found! Check 'jq' is in path "
+  print_ko
+  exit 1
+fi
+
+JET_BRAIN_PLUGINS_URL="https://plugins.jetbrains.com"
 
 install_plugin "${INTELLIJ_PLUGIN}"
 
