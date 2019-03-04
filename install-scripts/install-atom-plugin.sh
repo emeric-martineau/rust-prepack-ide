d72b585f7cc040eb4408cfed426962f6fe7018c7
Emeric MARTINEAU <11473190+bubulemaster@users.noreply.github.com>
2019-03-04 19:01:00 +0100

    Check if apm is installed

--- install-scripts/install-atom-plugin.sh
+++ install-scripts/install-atom-plugin.sh
@@ -4,6 +4,15 @@ BASEDIR="$(dirname ${REALPATH})"
 
 . "${BASEDIR}/../config.cfg"
 
+# Check if apm is installed
+if [ -z "$(command -v apm)" ]; then
+  . "${BASEDIR}/common.sh"
+
+  echo -n "Atom not found! Check 'apm' is in path "
+  print_ko
+  exit 1
+fi
+
 for pck_name in ${ATOM_PACKAGE}; do
   # Check if package is installed
   IS_INSTALLED=$(apm list --installed --bare | grep "${pck_name}")
